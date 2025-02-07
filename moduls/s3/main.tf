9ca2c2663c3314e4763d2d2a2df3777c7361603b
PeterChiz <buithienchi209@gmail.com>
2025-02-08 06:47:19 +0700

    delete dynamodb

--- moduls/s3/main.tf
+++ moduls/s3/main.tf
@@ -16,13 +16,13 @@ resource "aws_s3_bucket" "aws_s3_bucket" {
   bucket = "backend-s3-bedrock"
 }
 
-resource "aws_dynamodb_table" "terraform_locks" {
-  name         = "terraform-lock"
-  billing_mode = "PAY_PER_REQUEST"
-  hash_key     = "LockID"
+# resource "aws_dynamodb_table" "terraform_locks" {
+#   name         = "terraform-lock"
+#   billing_mode = "PAY_PER_REQUEST"
+#   hash_key     = "LockID"
 
-  attribute {
-    name = "LockID"
-    type = "S"
-  }
-}
+#   attribute {
+#     name = "LockID"
+#     type = "S"
+#   }
+# }
