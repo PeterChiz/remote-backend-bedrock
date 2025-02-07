59ecac7e3f32bd0530ca95a2d976d50ee181b07f
PeterChiz <buithienchi209@gmail.com>
2025-02-07 20:43:02 +0700

    fix add vm-user1

--- main.tf
+++ main.tf
@@ -19,16 +19,16 @@ provider "aws" {
   region = "us-east-1"
 }
 
-resource "aws_instance" "vm-user1" {
-  ami = "ami-04b6019d38ea93034"
-  instance_type = "t2.micro"
-  tags = {
-    Name ="vm-user1"
-  }
-}
-
 # Gắn IAM Policy với IAM Role
 resource "aws_iam_role_policy_attachment" "bedrock_policy_attachment" {
   role       = aws_iam_role.bedrock_access_role.name
   policy_arn = aws_iam_policy.bedrock_policy.arn
+}
+
+resource "aws_instance" "vm-user1" {
+  ami = "ami-0e532fbed6ef00604"
+  instance_type = "t2.micro"
+  tags = {
+    Name ="vm-user1"
+  }
 }
